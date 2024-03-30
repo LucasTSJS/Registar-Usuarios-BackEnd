3510afb1d06bbfb90b51d6a930d51e5e0b597f1a
LucasENG1 <131672706+LucasENG1@users.noreply.github.com>
2024-03-30 13:39:28 -0300

    Configurando variavel de prd

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const start = async () => {
     await app.register(routes); // Estou registrando a rota criada no Servidor!
     
     try {
-        await app.listen({ port: parseInt(process.env.PORT as string)})
+        await app.listen({ port: 9001 })
     } catch(err) {
         process.exit(1)
     }
